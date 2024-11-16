16cac125e640df10476b7dc67ae156787b5c2cde
ANURAG <forconnectanurag@gmail.com>
2024-11-16 11:18:20 +0530

    change image size

--- src/comp/About.jsx
+++ src/comp/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <p className='text-center text-2xl md:text-5xl font-semibold sm:pb-6'>About</p>
       <div className='grid grid-cols-1 xl:grid-cols-2 xl:flex gap-10 xl:gap-20'>
         <div className="lg:basis-[55%]" data-aos="flip-up" data-aos-duration="2000" >
-          <img className='mt-4 md:mt-0 rounded-lg shadow-md transform transition-all duration-500 hover:shadow-lg hover:scale-105' src="https://1drv.ms/i/c/2b8158e257563b12/IQS9vP4xExWdTqZki0u1qZ83AbJhjXpLc6uIApsqbVy_JZY?width=1280&height=720" alt="AboutImg" />
+          <img className='mt-4 md:mt-0 rounded-lg shadow-md transform transition-all duration-500 hover:shadow-lg hover:scale-105' src="https://1drv.ms/i/c/2b8158e257563b12/IQS9vP4xExWdTqZki0u1qZ83AbJhjXpLc6uIApsqbVy_JZY?width=1260&height=700" alt="AboutImg" />
 
         </div>
         <div className='lg:basis-[45%]'>
